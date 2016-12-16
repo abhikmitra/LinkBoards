fa9b1e4b81c1188dd1e9617d64b8df4623bdcadb
Naveen <naveen11aug@gmail.com>
2016-12-16 17:13:44 +0530

    adding cache for link reports

--- helpers/urlAnalyzer.js
+++ helpers/urlAnalyzer.js
@@ -7,7 +7,8 @@ var alchemy_language = watson.alchemy_language({
     // api_key: '8da9c35b8557581b14e0cb194ed349eba2a1321d'
     api_key: '1dc27ccd04df437d7e5fd3ca1aabf283ff7f0afc'
 });
-var extractParams = 'title,authors,keywords,concepts,taxonomy,doc-sentiment,doc-emotion,page-image,image-kw';
+var linkReports = require("../models/linkReports");
+var extractParams = 'title,authors,keywords,concepts,taxonomy,page-image,image-kw';
 
 function analyzeUrl(url, isTest) {
     var deferred = Q.defer();
@@ -17,17 +18,29 @@ function analyzeUrl(url, isTest) {
         url: url,
         showSourceText: 1
     };
-    alchemy_language.combined(parameters, function (err, response) {
-        if (err) {
-            console.log('error:', err);
-            deferred.reject("Unable to analyze URL using watson api ", err);
-        }
-        else {
-            console.log(JSON.stringify(response, null, 2));
-            response.requestURL = url;
-            deferred.resolve(response);
-        }
-    });
+    linkReports.getReport(url).then(function(response){
+        console.log("returning cached analysis report for url", url);
+        return deferred.resolve(response);
+    }, function(err){
+        console.log("Unable to find cached analysis report for url", url, err);
+        alchemy_language.combined(parameters, function (err, response) {
+            if (err) {
+                console.log('error:', err);
+                deferred.reject("Unable to analyze URL using watson api ", err);
+            }
+            else {
+                console.log(JSON.stringify(response, null, 2));
+                response.requestURL = url;
+                // add entry to cache
+                linkReports.upsertReport(url, response).then(function(report){
+                    console.log("successfully added cache entry for url", url)
+                }, function(err){
+                    console.log("Error adding cache entry for url", url);
+                })
+                deferred.resolve(response);
+            }
+        });
+    })
     return deferred.promise;
 }
 
---
+++ models/linkReports.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Naveen on 16/12/16.
+ */
+
+var Sequelize = require("sequelize");
+var sequelize = require("../sql/sequelize").sequelize;
+var Q = require('q');
+
+var LinkReports = sequelize.define('linkReports', {
+    id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement:true
+    },
+    link: {
+        allowNull: false,
+        type: Sequelize.STRING
+    },
+    report: {
+        allowNull: false,
+        type: Sequelize.TEXT
+    }
+}, {
+    freezeTableName: true, // Model tableName will be the same as the model name
+    getterMethods: {
+        report: function() {
+            return JSON.parse(this.getDataValue('report'));
+        }
+    },
+    setterMethods: {
+        report: function (value) {
+            this.setDataValue('report', JSON.stringify(value))
+        }
+    }
+});
+
+sequelize.sync().then(function () {
+    console.log("All Table Created");
+});
+
+LinkReports.upsertReport = function(link, report) {
+    var deferred = Q.defer();
+    return LinkReports
+        .create({
+            link: link,
+            report: report
+        })
+        .then(function (record) {
+            console.log("successfully saved cache entry for link ", link);
+            return deferred.resolve(record);
+        }, function(error) {
+            console.log("error saving cache entry for link ", link, error);
+            return deferred.reject(error);
+        });
+    return deferred.promise;
+};
+
+LinkReports.getReport = function (link) {
+    var deferred = Q.defer();
+    LinkReports
+        .findOne({where: {
+        link: link
+    }})
+        .then(function (row) {
+            if (row && row.length == 0) {
+                return deferred.reject("Cache entry not found for link", link)
+            }
+            deferred.resolve(row.report);
+        }, function (error) {
+            console.log("error retrieving cache entry for link", link, error);
+            return deferred.reject(error);
+        });
+    return deferred.promise;
+}
+
+module.exports = LinkReports;
